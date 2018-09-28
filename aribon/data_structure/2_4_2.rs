4bf3df42ec74820ae2dcab5be994fc1a8c9f1c37
muchomacho <tosh.un316@gmail.com>
2018-09-28 21:00:10 +0900

    Priority queue

---
+++ aribon/data_structure/2_4_2.rs
@@ -0,0 +1,72 @@
+#[allow(unused_imports)]
+use std::mem::swap;
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{VecDeque, HashSet, BTreeSet, BinaryHeap, HashMap};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, Write, BufReader, BufWriter};
+#[allow(unused_imports)]
+use std::io::prelude::BufRead;
+
+
+fn main() {
+    let _n = read::<usize>();
+    let l = read_vector::<i64>();
+    
+    let mut heap = BinaryHeap::<i64>::new();
+    for elem in l.iter() {
+        heap.push(*elem * (-1));
+    }
+
+    let mut cost = 0;
+    while heap.len() >= 2 {
+        let a = heap.pop().unwrap();
+        let b = heap.pop().unwrap();
+        cost += -(a + b);
+        heap.push(a + b);
+    }
+
+    println!("{}", cost);
+
+}
+
+#[allow(dead_code)]
+fn read<T>() -> T where T: std::str::FromStr, T::Err: std::fmt::Debug {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    return buf
+        .trim()
+        .parse()
+        .unwrap();
+}
+
+#[allow(dead_code)]
+fn read_vector<T>() -> Vec<T> where T: std::str::FromStr, T::Err: std::fmt::Debug {
+    let mut buf = String::with_capacity(100);
+    stdin().read_line(&mut buf).unwrap();
+    return buf
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+}
+
+#[allow(dead_code)]
+fn read_matrix<T>() -> Vec<Vec<T>> where T: std::str::FromStr, T::Err: std::fmt::Debug {
+    use std::io::prelude::*;
+    let stdin = stdin();
+    let mut reader = BufReader::with_capacity(100 * 1024, stdin);
+    let mut line = String::with_capacity(100);
+    let mut matrix: Vec<Vec<T>> = Vec::new();
+
+    while reader.read_line(&mut line).unwrap() > 0 {
+        matrix.push(line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect());
+        line.clear();
+    }
+
+    return matrix;
+}
