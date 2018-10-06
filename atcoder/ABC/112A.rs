14ec0087911344c98e1d8704e55bf02a7924ba04
muchomacho <tosh.un316@gmail.com>
2018-10-07 04:19:12 +0900

    Yarudake

---
+++ atcoder/ABC/112A.rs
@@ -0,0 +1,62 @@
+#[allow(unused_imports)]
+use std::mem::swap;
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{VecDeque, HashSet, BTreeSet, BinaryHeap, HashMap};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, Write, BufReader, BufWriter};
+#[allow(unused_imports)]
+use std::io::prelude::BufRead;
+ 
+ 
+fn main() {
+    let n = read::<usize>();
+    if n == 1 {
+        println!("Hello World");
+        return;
+    }
+    let a = read::<usize>();
+    let b = read::<usize>();
+    println!("{}", a + b);
+}
+ 
+#[allow(dead_code)]
+fn read<T>() -> T where T: std::str::FromStr, T::Err: std::fmt::Debug {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    return buf
+        .trim()
+        .parse()
+        .unwrap();
+}
+ 
+#[allow(dead_code)]
+fn read_vector<T>() -> Vec<T> where T: std::str::FromStr, T::Err: std::fmt::Debug {
+    let mut buf = String::with_capacity(100);
+    stdin().read_line(&mut buf).unwrap();
+    return buf
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+}
+ 
+#[allow(dead_code)]
+fn read_matrix<T>() -> Vec<Vec<T>> where T: std::str::FromStr, T::Err: std::fmt::Debug {
+    use std::io::prelude::*;
+    let stdin = stdin();
+    let mut reader = BufReader::with_capacity(100 * 1024, stdin);
+    let mut line = String::with_capacity(100);
+    let mut matrix: Vec<Vec<T>> = Vec::new();
+ 
+    while reader.read_line(&mut line).unwrap() > 0 {
+        matrix.push(line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect());
+        line.clear();
+    }
+ 
+    return matrix;
+}
---
+++ atcoder/ABC/112B.rs
@@ -0,0 +1,72 @@
+#[allow(unused_imports)]
+use std::mem::swap;
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{VecDeque, HashSet, BTreeSet, BinaryHeap, HashMap};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, Write, BufReader, BufWriter};
+#[allow(unused_imports)]
+use std::io::prelude::BufRead;
+ 
+ 
+fn main() {
+    let vec = read_vector::<i64>();
+    let (n, t) = (vec[0], vec[1]);
+ 
+    let mut heap = BinaryHeap::<i64>::new();
+    for _ in 0..n {
+        let vec = read_vector::<i64>();
+        let (a, b) = (vec[0], vec[1]);
+        if b > t {
+            continue;
+        }
+        heap.push(a * (-1));
+    }
+ 
+    if heap.len() == 0 {
+        println!("TLE");
+    } else {
+        println!("{}", heap.pop().unwrap() * (-1));
+    }
+}
+ 
+#[allow(dead_code)]
+fn read<T>() -> T where T: std::str::FromStr, T::Err: std::fmt::Debug {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    return buf
+        .trim()
+        .parse()
+        .unwrap();
+}
+ 
+#[allow(dead_code)]
+fn read_vector<T>() -> Vec<T> where T: std::str::FromStr, T::Err: std::fmt::Debug {
+    let mut buf = String::with_capacity(100);
+    stdin().read_line(&mut buf).unwrap();
+    return buf
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+}
+ 
+#[allow(dead_code)]
+fn read_matrix<T>() -> Vec<Vec<T>> where T: std::str::FromStr, T::Err: std::fmt::Debug {
+    use std::io::prelude::*;
+    let stdin = stdin();
+    let mut reader = BufReader::with_capacity(100 * 1024, stdin);
+    let mut line = String::with_capacity(100);
+    let mut matrix: Vec<Vec<T>> = Vec::new();
+ 
+    while reader.read_line(&mut line).unwrap() > 0 {
+        matrix.push(line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect());
+        line.clear();
+    }
+ 
+    return matrix;
+}
