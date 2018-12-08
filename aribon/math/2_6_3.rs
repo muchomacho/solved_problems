069d659b5584b5b398cbbcdf6d47623143c05405
muchomacho <tosh.un316@gmail.com>
2018-12-08 17:35:52 +0900

    Prime number check

---
+++ aribon/math/2_6_3.rs
@@ -0,0 +1,88 @@
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::prelude::BufRead;
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufReader, BufWriter, Write};
+#[allow(unused_imports)]
+use std::mem::swap;
+
+fn main() {
+    let n = read::<usize>();
+
+    if is_prime(n) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
+fn is_prime(n: usize) -> bool {
+    if n <= 2 {
+        return true;
+    }
+    let root_n = (n as f64).sqrt() as usize;
+    for i in 2..(root_n + 1) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
+#[allow(dead_code)]
+fn read<T>() -> T
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    return buf.trim().parse().unwrap();
+}
+
+#[allow(dead_code)]
+fn read_vector<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut buf = String::with_capacity(100);
+    stdin().read_line(&mut buf).unwrap();
+    return buf.split_whitespace().map(|s| s.parse().unwrap()).collect();
+}
+
+#[allow(dead_code)]
+fn read_matrix<T>() -> Vec<Vec<T>>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    use std::io::prelude::*;
+    let stdin = stdin();
+    let mut reader = BufReader::with_capacity(100 * 1024, stdin);
+    let mut line = String::with_capacity(100);
+    let mut matrix: Vec<Vec<T>> = Vec::new();
+
+    while reader.read_line(&mut line).unwrap() > 0 {
+        matrix.push(
+            line.trim()
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+        );
+        line.clear();
+    }
+
+    return matrix;
+}
+
+#[allow(dead_code)]
+fn read_chars() -> Vec<char> {
+    let stdin = stdin();
+    let mut buf = String::new();
+    let _bytes = stdin.read_line(&mut buf).unwrap();
+    return buf.chars().collect();
+}
