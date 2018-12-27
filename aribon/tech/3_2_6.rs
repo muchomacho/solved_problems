1bae397fc0727121005ba2c8d2597cef7bb01c30
muchomacho <tosh.un316@gmail.com>
2018-12-27 17:49:00 +0900

    Half exaustive search

---
+++ aribon/tech/3_2_6.rs
@@ -0,0 +1,100 @@
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::prelude::BufRead;
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufReader, BufWriter, Write};
+#[allow(unused_imports)]
+use std::mem::swap;
+#[allow(unused_imports)]
+use std::str::FromStr;
+
+
+fn main() {
+    let _n = read::<usize>();
+    let a = read_vector::<i64>();
+    let b = read_vector::<i64>();
+    let c = read_vector::<i64>();
+    let d = read_vector::<i64>();
+
+    let mut dict = BTreeMap::new();
+    for i in c.iter() {
+        for j in d.iter() {
+            let num = *i + *j;
+            if dict.contains_key(&num) {
+                *dict.get_mut(&num).unwrap() += 1;
+            } else {
+                dict.insert(num, 1);
+            }
+        }
+    }
+
+    let mut answer = 0;
+    for i in a.iter() {
+        for j in b.iter() {
+            let num = - (*i + *j);
+            if let Some(val) = dict.get(&num) {
+                answer += *val;
+            }
+        }
+    }
+
+    println!("{}", answer);
+}
+
+#[allow(dead_code)]
+fn read<T>() -> T
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    return buf.trim().parse().unwrap();
+}
+
+#[allow(dead_code)]
+fn read_vector<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut buf = String::with_capacity(100);
+    stdin().read_line(&mut buf).unwrap();
+    return buf.split_whitespace().map(|s| s.parse().unwrap()).collect();
+}
+
+#[allow(dead_code)]
+fn read_matrix<T>() -> Vec<Vec<T>>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    use std::io::prelude::*;
+    let stdin = stdin();
+    let mut reader = BufReader::with_capacity(100 * 1024, stdin);
+    let mut line = String::with_capacity(100);
+    let mut matrix: Vec<Vec<T>> = Vec::new();
+
+    while reader.read_line(&mut line).unwrap() > 0 {
+        matrix.push(
+            line.trim()
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+        );
+        line.clear();
+    }
+
+    return matrix;
+}
+
+#[allow(dead_code)]
+fn read_chars() -> Vec<char> {
+    let stdin = stdin();
+    let mut buf = String::new();
+    let _bytes = stdin.read_line(&mut buf).unwrap();
+    return buf.trim().chars().collect();
+}
