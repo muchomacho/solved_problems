3c6b0f977d9ad73639ef7e1f3ea23f8b59f46269
muchomacho <tosh.un316@gmail.com>
2019-01-25 00:17:45 +0900

    Refactor code

--- aribon/dp/3_4_2.java
+++ aribon/dp/3_4_2.java
@@ -1,17 +1,24 @@
 import java.util.*;
+import java.text.MessageFormat;
+
 public class Main {
+	static int n, m, a, b;
+	static int t[];
+	static int adj[][];
+	static double dp[][];
+
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int t[] = new int[n];
+		n = sc.nextInt();
+		m = sc.nextInt();
+		a = sc.nextInt();
+		b = sc.nextInt();
+		t = new int[n];
 		for (int i = 0; i < n; i++) {
 			t[i] = sc.nextInt();
 		}
 		// read adjacency matrix
-		int adj[][] = new int[m][m];
+		adj = new int[m][m];
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < m; j++) {
 				String s = sc.next();
@@ -23,46 +30,47 @@ public class Main {
 			}
 		}
 
-		double dp[][] = new double[1 << n][m];
+		dp = new double[1 << n][m];
 		for (int i = 0; i < 1 << n; i++) {
 			for (int j = 0; j < m; j++) {
 				dp[i][j] = -1.0;
 			}
 		}
 		
-		double ans = rec(t, dp, adj, 0, a, b);
+		double ans = rec(0, a);
 		if (ans < Double.MAX_VALUE) {
-			System.out.println(ans);
+			final String ans_string = String.format("%1$.3f", ans);
+			System.out.println(ans_string);
 		} else {
 			System.out.println("Impossible");
 		}
 		sc.close();
 	}
 
-	static double rec(int[] t, double[][] dp, int[][] adj, int S, int start, int end) {
-		if (dp[S][start] >= 0.0) {
-			return dp[S][start];
+	static double rec(int S, int v) {
+		if (dp[S][v] >= 0.0) {
+			return dp[S][v];
 		}
-		if (start == end) {
-			dp[S][start] = 0.0;
+		if (v == b) {
+			dp[S][v] = 0.0;
 			return 0.0;
 		}
 		double min_cost = Double.MAX_VALUE;
 		for (int i = 0; i < adj.length; i++) {
-			if (adj[start][i] == Integer.MAX_VALUE) {
+			if (adj[v][i] == Integer.MAX_VALUE) {
 				continue;
 			}
 			for (int j = 0; j < t.length; j++) {
 				if ((S & (1 << j)) >> j == 1) {
 					continue;
 				}
-				double cost = rec(t, dp, adj, S | (1 << j), i, end);
+				double cost = rec(S | (1 << j), i);
 				if (cost < Double.MAX_VALUE) {
-					min_cost = Math.min(min_cost, cost + ((double) adj[start][i]) / ((double) t[j]));
+					min_cost = Math.min(min_cost, cost + ((double) adj[v][i]) / ((double) t[j]));
 				}
 			}
 		}
-		dp[S][start] = min_cost;
+		dp[S][v] = min_cost;
 		return min_cost;
 	}
 }
